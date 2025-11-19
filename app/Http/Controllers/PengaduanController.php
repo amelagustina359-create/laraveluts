02b8f63d682a27b1ae4a3d72891bc525979536f8
Amelia Agustina <amelagustina359@gmail.com>
2025-11-19 10:10:42 +0700

    hai

--- app/Http/Controllers/PengaduanController.php
+++ app/Http/Controllers/PengaduanController.php
@@ -10,8 +10,8 @@ class PengaduanController extends Controller
         // ambil semua pengaduan terbaru dulu
         $pengaduan = \App\Models\Pengaduan::orderBy('created_at', 'desc')->get();
 
-        // pastikan nama view sesuai: gunakan 'pengaduan.daftar-pengaduan' jika file itu yang Anda pakai
-        return view('pengaduan.daftar-pengaduan', compact('pengaduan'));
+        // Tampilkan view index yang sudah dibuat di resources/views/pengaduan/index.blade.php
+        return view('pengaduan.index', compact('pengaduan'));
     }
 }
 
